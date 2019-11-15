77f36f8e3fd9483bef84b1584316a8a4546f3ea5
Zoltan <fekete.zoltan@live.com>
2019-11-15 16:47:06 +0100

    Initial Project Add

---
+++ src/main/java/pro/zoltanfekete/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pro.zoltanfekete.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicApplication.class, args);
+    }
+
+}
