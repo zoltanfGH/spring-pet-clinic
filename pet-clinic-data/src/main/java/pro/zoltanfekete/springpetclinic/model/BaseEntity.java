2b982f5010cb8ffa974984718026dd31b6fb0042
Zoltan <fekete.zoltan@live.com>
2019-11-22 13:29:55 +0100

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/BaseEntity.java
@@ -3,9 +3,11 @@ package pro.zoltanfekete.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity {
+public class BaseEntity implements Serializable {
 
-    private String name;
+    private Long id;
 }
