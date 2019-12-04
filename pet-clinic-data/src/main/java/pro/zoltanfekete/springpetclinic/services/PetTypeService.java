a7270a0672a6c4f6ccc3654887dd70be4748da35
Zoltan <fekete.zoltan@live.com>
2019-12-04 11:27:26 +0100

    Added PetTypeService and PetTypeServiceMap. Closes #29

---
+++ pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pro.zoltanfekete.springpetclinic.services;
+
+import pro.zoltanfekete.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
