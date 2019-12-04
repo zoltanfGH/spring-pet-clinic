1a4f33d6dc80a4981a39da8989d6198f8c91e427
Zoltan <fekete.zoltan@live.com>
2019-12-04 10:38:37 +0100

    Added Visits. Updated Owner with Pet Relationship. Closes #26

--- pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Owner.java
@@ -1,4 +1,13 @@
 package pro.zoltanfekete.springpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
 public class Owner extends Person {
+
+    @Getter
+    @Setter
+    private Set<Pet> pets;
 }
