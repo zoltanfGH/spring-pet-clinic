af9debcfcae13ea331ffd991be69d381100cccab
Zoltan <fekete.zoltan@live.com>
2019-12-09 17:47:54 +0100

    Updated Visit with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,4 +27,7 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 }
