a935007192775e065e1d5af7a8a7f2e9f9330866
Zoltan <fekete.zoltan@live.com>
2019-12-09 17:25:20 +0100

    Updated Owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pro/zoltanfekete/springpetclinic/model/Owner.java
@@ -3,15 +3,22 @@ package pro.zoltanfekete.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+   @Column(name = "address")
    private String address;
+   @Column(name = "city")
    private String city;
+   @Column(name = "telephone")
    private String telephone;
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "owner")
    private Set<Pet> pets = new HashSet<>();
 }
